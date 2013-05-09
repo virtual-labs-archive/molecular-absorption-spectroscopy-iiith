2eacfb2d6ce68d49d5fb38c34903346ef7318d2a
swathy <swathy@18f66021-0b22-4fe9-9189-e58fb6d02c8e>
2013-05-09 06:19:29 +0000

    scripts modified
    git-svn-id: file:///labs/ccnsb03/svn/Repo_lab_3@10 18f66021-0b22-4fe9-9189-e58fb6d02c8e

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,14 +1,38 @@
+#!/bin/bash
+
+# init script for the molecular absorption spectroscopy lab
+ 
+#init installs dependencies, runs the make file and creates the build
+#dependecies.txt is the file where all the packages required to run
+# the labs should be entered
+#it automatically runs the make file, builds the lab and copies the lab
+#to /var/www
+
+#usage
+
+#init.sh takes the dependecies.txt as an input and installs all the
+#required software
+#should be invoked using the following command:
+#sh scripts/init.sh scripts/dependencies.txt
+
+#update the system packages
 apt-get update
-#Read the file line by line and install
+
+#$1 is the shell variable for command-line argument
 FILENAME=$1
+
+#reads the given file as an argument to script line by line and install packages
 cat $FILENAME | while read LINE
 do
 apt-get install -y $LINE
 done
 
+#changes directory  to run the makefile
 cd src
 make all
 cd ..
+
+#copies build to localhost
 cp -r build/ /var/www/
 
 
